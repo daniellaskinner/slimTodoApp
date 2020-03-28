e770caf9425560f046279bc4024586f1918b925e
daniellaskinner <daniellaskinner120@gmail.com>
2020-03-28 16:12:41 +0000

    finished delete functionality and added background image

--- public/js/deleteTodo.js
+++ public/js/deleteTodo.js
@@ -1,12 +1,21 @@
-// document.getElementById(deleteTodo)
 
-document.getElementById('test').addEventListener("click", ()=> {
-    fetch('/todos', {
-        method: 'DELETE',
-        body: JSON.stringify({"id":"11"}),
-        headers: {
-            "Content-Type": "application/json"
-        }
+//grab button by class name
+let deleteButtons = document.getElementsByClassName('btn-delete');
+
+
+//returns a html collection of buttons so foreach loop through buttons to add event listener
+Array.from(deleteButtons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let id = e.target.dataset.id;
+        let todo = e.target.dataset.todo;
+        fetch('/todos', {
+            method: 'DELETE',
+            body: JSON.stringify({"id": id}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        alert('You deleted: ' + todo);
+        window.location.href = "http://localhost:8080/todos";
     })
-})
-
+});
